5e48c7a18c40ec99dfa78591a1512b2c4c37ea5a
Stephen Piccolo <goldfish1434@yahoo.com>
2012-01-25 22:28:59 -0700

    No comment

--- Internals/Java/transformation/tcga/TwoYearContinuousDependentValueTransformer.java
+++ Internals/Java/transformation/tcga/TwoYearContinuousDependentValueTransformer.java
@@ -66,6 +66,23 @@
 // You should have received a copy of the GNU General Public License
 // along with ML-Flex. If not, see <http://www.gnu.org/licenses/>.
 
+// Copyright 2011 Stephen Piccolo
+// 
+// This file is part of ML-Flex.
+// 
+// ML-Flex is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// ML-Flex is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with ML-Flex. If not, see <http://www.gnu.org/licenses/>.
+
 package mlflex.transformation.tcga;
 
 import mlflex.transformation.ContinuousDependentValueBinaryTransformer;
