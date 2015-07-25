6b1449ca67aacc1cdd1aa417621908bec935b7a1
Stephen Piccolo <goldfish1434@yahoo.com>
2015-07-25 15:17:49 -0600

    No comment

--- Internals/R/Predict.R
+++ Internals/R/Predict.R
@@ -1,3 +1,23 @@
+# THIS SOURCE CODE IS SUPPLIED "AS IS" WITHOUT WARRANTY OF ANY KIND, AND ITS AUTHOR AND THE JOURNAL OF MACHINE LEARNING RESEARCH (JMLR) AND JMLR'S PUBLISHERS AND DISTRIBUTORS, DISCLAIM ANY AND ALL WARRANTIES, INCLUDING BUT NOT LIMITED TO ANY IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND ANY WARRANTIES OR NON INFRINGEMENT. THE USER ASSUMES ALL LIABILITY AND RESPONSIBILITY FOR USE OF THIS SOURCE CODE, AND NEITHER THE AUTHOR NOR JMLR, NOR JMLR'S PUBLISHERS AND DISTRIBUTORS, WILL BE LIABLE FOR DAMAGES OF ANY KIND RESULTING FROM ITS USE. Without lim- iting the generality of the foregoing, neither the author, nor JMLR, nor JMLR's publishers and distributors, warrant that the Source Code will be error-free, will operate without interruption, or will meet the needs of the user.
+# 
+# --------------------------------------------------------------------------
+# 
+# Copyright 2011 Stephen Piccolo
+# 
+# This file is part of ML-Flex.
+# 
+# ML-Flex is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# 
+# ML-Flex is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with ML-Flex. If not, see <http://www.gnu.org/licenses/>.
 
 # THIS SOURCE CODE IS SUPPLIED "AS IS" WITHOUT WARRANTY OF ANY KIND, AND ITS AUTHOR AND THE JOURNAL OF MACHINE LEARNING RESEARCH (JMLR) AND JMLR'S PUBLISHERS AND DISTRIBUTORS, DISCLAIM ANY AND ALL WARRANTIES, INCLUDING BUT NOT LIMITED TO ANY IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND ANY WARRANTIES OR NON INFRINGEMENT. THE USER ASSUMES ALL LIABILITY AND RESPONSIBILITY FOR USE OF THIS SOURCE CODE, AND NEITHER THE AUTHOR NOR JMLR, NOR JMLR'S PUBLISHERS AND DISTRIBUTORS, WILL BE LIABLE FOR DAMAGES OF ANY KIND RESULTING FROM ITS USE. Without lim- iting the generality of the foregoing, neither the author, nor JMLR, nor JMLR's publishers and distributors, warrant that the Source Code will be error-free, will operate without interruption, or will meet the needs of the user.
 # 
@@ -27,7 +47,11 @@ trainTestSvm = function(trainData, testData, kernelType)
   trainClasses = as.factor(trainData[,ncol(trainData)])
   trainData = trainData[,-ncol(trainData)]
 
-  best.parameters = tune.svm(trainData, trainClasses, kernel=kernelType, probability=TRUE, cost=c(0.01,1,100,1000), tunecontrol = tune.control(sampling="cross"))$best.parameters
+  cross = 10
+  if (length(trainClasses) < 10)
+    cross = length(trainClasses)
+
+  best.parameters = tune.svm(trainData, trainClasses, kernel=kernelType, probability=TRUE, cost=c(0.01,1,100,1000), tunecontrol = tune.control(sampling="cross", cross=cross))$best.parameters
   model = svm(trainData, trainClasses, type='C', kernel=kernelType, probability=TRUE, cost=best.parameters[,"cost"])
 
   predictResult = predict(model, testData, probability=TRUE)
@@ -59,8 +83,8 @@ trainDataFilePath = commandArgs()[8]
 testDataFilePath = commandArgs()[9]
 outputFilePath = commandArgs()[10]
 
-trainData = read.table(trainDataFilePath, sep="\t", row.names=NULL, stringsAsFactors=FALSE, header=TRUE, quote="\"")
-testData = read.table(testDataFilePath, sep="\t", row.names=NULL, stringsAsFactors=FALSE, header=TRUE, quote="\"")
+trainData = read.table(trainDataFilePath, sep="\t", row.names=NULL, stringsAsFactors=FALSE, header=TRUE, quote="\"", check.names=FALSE)
+testData = read.table(testDataFilePath, sep="\t", row.names=NULL, stringsAsFactors=FALSE, header=TRUE, quote="\"", check.names=FALSE)
 
 saveResults = function(results)
 {
