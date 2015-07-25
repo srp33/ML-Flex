63d02dfdf14081c9b1c444aa7d884dca10029644
Stephen Piccolo <goldfish1434@yahoo.com>
2015-07-25 15:20:19 -0600

    No comment

--- Internals/Java/core/Settings.java
+++ Internals/Java/core/Settings.java
@@ -19,28 +19,6 @@
 // You should have received a copy of the GNU General Public License
 // along with ML-Flex. If not, see <http://www.gnu.org/licenses/>.
 
-
-// THIS SOURCE CODE IS SUPPLIED "AS IS" WITHOUT WARRANTY OF ANY KIND, AND ITS AUTHOR AND THE JOURNAL OF MACHINE LEARNING RESEARCH (JMLR) AND JMLR'S PUBLISHERS AND DISTRIBUTORS, DISCLAIM ANY AND ALL WARRANTIES, INCLUDING BUT NOT LIMITED TO ANY IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND ANY WARRANTIES OR NON INFRINGEMENT. THE USER ASSUMES ALL LIABILITY AND RESPONSIBILITY FOR USE OF THIS SOURCE CODE, AND NEITHER THE AUTHOR NOR JMLR, NOR JMLR'S PUBLISHERS AND DISTRIBUTORS, WILL BE LIABLE FOR DAMAGES OF ANY KIND RESULTING FROM ITS USE. Without lim- iting the generality of the foregoing, neither the author, nor JMLR, nor JMLR's publishers and distributors, warrant that the Source Code will be error-free, will operate without interruption, or will meet the needs of the user.
-// 
-// --------------------------------------------------------------------------
-// 
-// Copyright 2011 Stephen Piccolo
-// 
-// This file is part of ML-Flex.
-// 
-// ML-Flex is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// any later version.
-// 
-// ML-Flex is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with ML-Flex. If not, see <http://www.gnu.org/licenses/>.
-
 package mlflex.core;
 
 import mlflex.dataprocessors.AbstractDataProcessor;
@@ -291,9 +269,9 @@ public class Settings
         return GetNumberClassificationCombinations(true) > 1;
     }
 
-    /** Indicates whether this is a training/testing experiment.
+    /** Indicates whether this is a training/Action experiment.
      *
-     * @return Whether this is a training/testing experiment
+     * @return Whether this is a training/Action experiment
      * @throws Exception
      */
     public static boolean IsTrainTestExperiment() throws Exception
--- Internals/Java/core/TrainTestValidationAssignments.java
+++ Internals/Java/core/TrainTestValidationAssignments.java
@@ -19,28 +19,6 @@
 // You should have received a copy of the GNU General Public License
 // along with ML-Flex. If not, see <http://www.gnu.org/licenses/>.
 
-
-// THIS SOURCE CODE IS SUPPLIED "AS IS" WITHOUT WARRANTY OF ANY KIND, AND ITS AUTHOR AND THE JOURNAL OF MACHINE LEARNING RESEARCH (JMLR) AND JMLR'S PUBLISHERS AND DISTRIBUTORS, DISCLAIM ANY AND ALL WARRANTIES, INCLUDING BUT NOT LIMITED TO ANY IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND ANY WARRANTIES OR NON INFRINGEMENT. THE USER ASSUMES ALL LIABILITY AND RESPONSIBILITY FOR USE OF THIS SOURCE CODE, AND NEITHER THE AUTHOR NOR JMLR, NOR JMLR'S PUBLISHERS AND DISTRIBUTORS, WILL BE LIABLE FOR DAMAGES OF ANY KIND RESULTING FROM ITS USE. Without lim- iting the generality of the foregoing, neither the author, nor JMLR, nor JMLR's publishers and distributors, warrant that the Source Code will be error-free, will operate without interruption, or will meet the needs of the user.
-// 
-// --------------------------------------------------------------------------
-// 
-// Copyright 2011 Stephen Piccolo
-// 
-// This file is part of ML-Flex.
-// 
-// ML-Flex is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// any later version.
-// 
-// ML-Flex is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with ML-Flex. If not, see <http://www.gnu.org/licenses/>.
-
 package mlflex.core;
 
 import mlflex.dataprocessors.AbstractDataProcessor;
@@ -49,7 +27,7 @@ import mlflex.helper.ListUtilities;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-/** This class stores assignments of data instances to either training or testing groups. It is used when cross-validation is NOT performed (or you can think of it as one-fold cross-validation).
+/** This class stores assignments of data instances to either training or Action groups. It is used when cross-validation is NOT performed (or you can think of it as one-fold cross-validation).
  * @author Stephen Piccolo
  */
 public class TrainTestValidationAssignments extends CrossValidationAssignments
@@ -97,7 +75,7 @@ public class TrainTestValidationAssignments extends CrossValidationAssignments
     public ArrayList<String> GetTestIDs(int fold) throws Exception
     {
         if (fold > 1)
-            throw new Exception("When performing training and testing, only fold 0 is allowed. Fold " + fold + " was specified.");
+            throw new Exception("When performing training and Action, only fold 0 is allowed. Fold " + fold + " was specified.");
 
         return Assignments.get(2);
     }
@@ -106,7 +84,7 @@ public class TrainTestValidationAssignments extends CrossValidationAssignments
     public ArrayList<String> GetTrainIDs(int fold) throws Exception
     {
         if (fold > 1)
-            throw new Exception("When performing training and testing, only fold 0 is allowed. Fold " + fold + " was specified.");
+            throw new Exception("When performing training and Action, only fold 0 is allowed. Fold " + fold + " was specified.");
 
         return FilterTrainIDs(Assignments.get(1));
     }
